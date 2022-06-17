4bb950912bb563b8970e7a1bcd98c12824f19f4b
C.J. Cline <ccline140@yahoo.com>
2022-06-16 21:54:35 -0500

    Worked At Home
    Added prop-types

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import ShoppingCart from '../images/icon-cart.svg';
+import React from "react";
+import propTypes from "prop-types";
+import ShoppingCart from "../images/icon-cart.svg";
 
-function Cart(props) {
+function Cart({ itemsCount, active }) {
   return (
-    <div className='Navbar--cart'>
-      <p className='Navbar--cart--items'>{props.itemsCount}</p>
-      <img clasName='Navbar--cart--icon' src={ShoppingCart} alt='Shopping Cart' />
+    <div className="Navbar--cart">
+      <p className="Navbar--cart--items">{itemsCount}</p>
+      <img
+        className="Navbar--cart--icon" 
+        src={ShoppingCart}
+        alt="Shopping Cart"
+      />
     </div>
   );
 }
+
+Cart.propTypes = {
+  itemsCount: propTypes.number.isRequired,
+  active: propTypes.bool.isRequired
+};
 
 export default Cart;
